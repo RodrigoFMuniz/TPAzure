edb8efacf61b774ed6e2006254baafea337f828d
Rodrigo <rodrigo.muniz.dev@gmail.com=>
2021-05-11 22:44:49 -0300

    Criação de Views via EFCore, CFriação de controllers, Context e Migration

--- TPAzure/Startup.cs
+++ TPAzure/Startup.cs
@@ -55,7 +55,7 @@ namespace TPAzure
             {
                 endpoints.MapControllerRoute(
                     name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                    pattern: "{controller=PaisEntities}/{action=Index}/{id?}");
             });
         }
     }
